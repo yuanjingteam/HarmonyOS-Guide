75a60ba026b87d73f8c102a63f88d45b41ae0fc5
yuanjing <marchsoft@163.com>
2024-07-30 15:46:50 +0800

    deploy

--- src/.vuepress/.temp/pages/home.html.vue
+++ src/.vuepress/.temp/pages/home.html.vue
@@ -8,6 +8,7 @@
 <h2 id="常见问题" tabindex="-1"><a class="header-anchor" href="#常见问题"><span>常见问题</span></a></h2>
 <ul>
 <li><a href="https://blog.csdn.net/qq_44103359/article/details/138468933" target="_blank" rel="noopener noreferrer">鸿蒙4.0和鸿蒙Next有什么区别？</a></li>
+<li><a href="https://hmxt.org/f/harmonyos-next%E7%89%88%E6%9C%AC%E6%9B%B4%E6%96%B0%E8%AE%A1%E5%88%92" target="_blank" rel="noopener noreferrer">harmonyos-next版本更新计划</a></li>
 </ul>
 </div></template>
 
--- src/home.md
+++ src/home.md
@@ -14,5 +14,6 @@ title: HarmonyGuide
 
 
 - [鸿蒙4.0和鸿蒙Next有什么区别？](https://blog.csdn.net/qq_44103359/article/details/138468933)
+- [harmonyos-next版本更新计划](https://hmxt.org/f/harmonyos-next%E7%89%88%E6%9C%AC%E6%9B%B4%E6%96%B0%E8%AE%A1%E5%88%92)
 
 
